c51b489ba62759d24c02039b9e2dd125c4665143
BMATHEW <BMATHEW@BMATHEW>
2016-05-06 13:59:28 -0400

    Add project files.

---
+++ src/eStore/Models/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace eStore.Models
+{
+    public partial class Car
+    {
+        public Car()
+        {
+        }
+
+        [StringLength(15)]
+        public string Id { get; set; }
+        [Required]
+        public int CarClassId { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Make { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Model { get; set; }
+        [Required]
+        public int Year { get; set; }
+        [Required]
+        public int NumofSeats { get; set; }
+        [Required]
+        public int NumOfDoors { get; set; }
+        [Required]
+        public int NumOfCylinders { get; set; }
+        [Required]
+        public int SafetyRating { get; set; }
+        [Required]
+        public double GasolineCapacity { get; set; }
+        [Required]
+        public bool Cabriolet { get; set; }
+        [Required]
+        public int HorsePower { get; set; }
+        [Required]
+        public double MPG { get; set; }
+        [Required]
+        public bool FourWheelDrive { get; set;}
+        [Required] 
+        [StringLength(10)]
+        public string Transmission { get; set; }
+        [Required]
+        public bool Hybrid { get; set; }
+
+
+        [Required]
+        [StringLength(20)]
+        public string GraphicName { get; set; }
+        [Column(TypeName = "money")]
+        public decimal CostPrice { get; set; }
+        [Column(TypeName = "money")]
+        public decimal MSRP { get; set; }
+        public int QtyOnHand { get; set; }
+        public int QtyOnBackOrder { get; set; }
+        [StringLength(2000)]
+        public string Description { get; set; }
+        [Column(TypeName = "timestamp")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        [MaxLength(8)]
+        public byte[] Timer { get; set; }
+    }
+}
